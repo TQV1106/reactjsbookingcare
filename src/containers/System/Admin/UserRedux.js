3db295b74caead3128874b6d3918ed37b0005b95
viet11062001 <viet11062001@gmail.com>
2023-04-20 13:52:59 +0700

    upload img base64

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import { getAllCodeService } from "../../../services/userService";
-import { LANGUAGES, CRUD_ACTIONS } from "../../../utils";
+import { LANGUAGES, CRUD_ACTIONS, CommonUtils } from "../../../utils";
 import * as actions from "../../../store/actions";
 import "./UserRedux.scss";
 import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css";
 import TableManageUser from "./TableManageUser";
+
 class UserRedux extends Component {
   constructor(props) {
     super(props);
@@ -95,18 +96,21 @@ class UserRedux extends Component {
         role: arrRoles && arrRoles.length > 0 ? arrRoles[0].key : "",
         avatar: "",
         action: CRUD_ACTIONS.CREATE,
+        previewImgeUrl: "",
       });
     }
   }
 
-  handleOnChangeImage = (event) => {
+  handleOnChangeImage = async (event) => {
     let data = event.target.files;
     let file = data[0];
     if (file) {
+      let base64 = await CommonUtils.getBase64(file);
+      console.log("check img: ", base64);
       let objectUrl = URL.createObjectURL(file);
       this.setState({
         previewImgeUrl: objectUrl,
-        avatar: file,
+        avatar: base64,
       });
     }
   };
@@ -133,6 +137,7 @@ class UserRedux extends Component {
         gender: this.state.gender,
         roleId: this.state.role,
         positionId: this.state.position,
+        avatar: this.state.avatar,
       });
     }
     if (action === CRUD_ACTIONS.EDIT) {
@@ -147,7 +152,7 @@ class UserRedux extends Component {
         gender: this.state.gender,
         roleId: this.state.role,
         positionId: this.state.position,
-        // avatar: this.state.avatar,
+        avatar: this.state.avatar,
       });
     }
   };
@@ -180,6 +185,10 @@ class UserRedux extends Component {
   };
 
   handleEditUserFromParent = (user) => {
+    let imageBase64 = "";
+    if (user.image) {
+      imageBase64 = new Buffer(user.image, "base64").toString("binary");
+    }
     this.setState({
       email: user.email,
       password: "hardcode",
@@ -191,6 +200,7 @@ class UserRedux extends Component {
       position: user.positionId,
       role: user.roleId,
       avatar: "",
+      previewImgeUrl: imageBase64,
       action: CRUD_ACTIONS.EDIT,
       userEditId: user.id,
     });
--- src/utils/CommonUtils.js
+++ src/utils/CommonUtils.js
@@ -1,8 +1,12 @@
 class CommonUtils {
-    static isNumber1 (number) {
-        if (number === 1) return true;
-        return false;
-    }
+  static getBase64(file) {
+    return new Promise((resole, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resole(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  }
 }
 
 export default CommonUtils;
