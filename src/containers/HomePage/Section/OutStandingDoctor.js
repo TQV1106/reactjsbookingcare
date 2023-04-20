998e83a4a98c0ebca874142630ef3533e500c175
viet11062001 <viet11062001@gmail.com>
2023-04-20 16:56:41 +0700

    load-infor-doctor

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -1,117 +1,96 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
 import Slider from "react-slick";
+import * as actions from "../../../store/actions";
+import { LANGUAGES } from "../../../utils";
 
 class OutStandingDoctor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrDoctors: [],
+    };
+  }
 
-    render() {
-    
-        return (
-            <div className='section-share section-outstanding-doctor section-specialty'>
-            <div className='section-container'>
-                <div className='section-header'>
-                    <span className='title-section'>Bác sĩ nổi bật tuần qua</span>
-                    <button className='btn-section'>xem thêm</button>
-                </div>
-                <div className='section-body '>
-                <Slider {...this.props.settings}>
-                <div className='section-customize'>
-                    <div className='customize-border'>
-                    <div className='outer-bg'>
-                    <div className='bg-image section-outstanding-doctor'></div>
-
-                    </div>
-                    <div className='position text-center'>
-                    <div className=''>Giáo sư tiến sĩ</div>
-                    <div className=''>Cơ xương khớp 1</div>
-                    </div>
-                    </div>
-                </div>
-                <div className='section-customize'>
-                    <div className='customize-border'>
-                    <div className='outer-bg'>
-                    <div className='bg-image section-outstanding-doctor'></div>
-
-                    </div>
-                    <div className='position text-center'>
-                    <div className=''>Giáo sư tiến sĩ</div>
-                    <div className=''>Cơ xương khớp 2</div>
-                    </div>
-                    </div>
-                </div>
-                <div className='section-customize'>
-                <div className='customize-border'>
-
-                    <div className='outer-bg'>
-                    <div className='bg-image section-outstanding-doctor'></div>
-
-                    </div>
-                    <div className='position text-center'>
-                    <div className=''>Giáo sư tiến sĩ</div>
-                    <div className=''>Cơ xương khớp 3</div>
-                    </div>
-                    </div>
-                </div>
-                <div className='section-customize'>
-                <div className='customize-border'>
-
-                    <div className='outer-bg'>
-                    <div className='bg-image section-outstanding-doctor'></div>
-
-                    </div>
-                    <div className='position text-center'>
-                    <div className=''>Giáo sư tiến sĩ</div>
-                    <div className=''>Cơ xương khớp 4</div>
-                    </div>
-                    </div>
-                </div>
-                <div className='section-customize'>
-                <div className='customize-border'>
-
-                    <div className='outer-bg'>
-                    <div className='bg-image section-outstanding-doctor'></div>
-
-                    </div>
-                    <div className='position text-center'>
-                    <div className=''>Giáo sư tiến sĩ</div>
-                    <div className=''>Cơ xương khớp 5</div>
-                    </div>
-                    </div>
-                </div>
-                <div className='section-customize'>
-                <div className='customize-border'>
-
-                    <div className='outer-bg'>
-                    <div className='bg-image section-outstanding-doctor'></div>
-
-                    </div>
-                    <div className='position text-center'>
-                    <div className=''>Giáo sư tiến sĩ</div>
-                    <div className=''>Cơ xương khớp 6</div>
-                    </div>
-                    </div>
-                </div>
-                
-            </Slider>
-                </div>
-           
-            </div>
-           </div>
-        );
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
+      this.setState({
+        arrDoctors: this.props.topDoctorsRedux,
+      });
     }
+  }
+  componentDidMount() {
+    this.props.loadTopDoctor();
+  }
 
+  render() {
+    let arrDoctors = this.state.arrDoctors;
+    let { language } = this.props;
+    arrDoctors = arrDoctors.concat(arrDoctors).concat(arrDoctors);
+
+    return (
+      <div className="section-share section-outstanding-doctor section-specialty">
+        <div className="section-container">
+          <div className="section-header">
+            <span className="title-section">Bác sĩ nổi bật tuần qua</span>
+            <button className="btn-section">xem thêm</button>
+          </div>
+          <div className="section-body ">
+            <Slider {...this.props.settings}>
+              {arrDoctors &&
+                arrDoctors.length > 0 &&
+                arrDoctors.map((item, index) => {
+                  let imageBase64 = "";
+                  if (item.image) {
+                    imageBase64 = new Buffer(item.image, "base64").toString(
+                      "binary"
+                    );
+                  }
+
+                  let nameVi = `${item.positionData.valueVi}, ${item.firstName} ${item.lastName}`;
+                  let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
+                  return (
+                    <div className="section-customize" key={index}>
+                      <div className="customize-border">
+                        <div className="outer-bg">
+                          <div
+                            className="bg-image section-outstanding-doctor"
+                            style={{
+                              backgroundImage: `url(${imageBase64})`,
+                            }}
+                          ></div>
+                        </div>
+                        <div className="position text-center">
+                          <div className="">
+                            {language === LANGUAGES.VI ? nameVi : nameEn}
+                          </div>
+                          <div className="">Cơ xương khớp 1</div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    topDoctorsRedux: state.admin.topDoctors,
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadTopDoctor: () => dispatch(actions.fetchTopDoctor()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
