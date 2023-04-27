7db03bc42ad8e0530f051d12231551705d5aeb96
viet11062001 <viet11062001@gmail.com>
2023-04-27 17:18:31 +0700

    load dynamic data schedule

--- src/services/userService.js
+++ src/services/userService.js
@@ -47,6 +47,13 @@ const getDetaiInforDoctor = (id) => {
 const saveBulkScheduleDoctor = (data) => {
   return axios.post("/api/bulk-create-schedule", data);
 };
+
+const getScheduleDoctorByDate = (doctorId, date) => {
+  return axios.get(
+    `/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`
+  );
+};
+
 export {
   handleLoginApi,
   getAllUsers,
@@ -59,4 +66,5 @@ export {
   saveDetailDoctorService,
   getDetaiInforDoctor,
   saveBulkScheduleDoctor,
+  getScheduleDoctorByDate,
 };
